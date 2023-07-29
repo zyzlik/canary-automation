3e6a537dc8cacda71623d62621decf63bd5f2fff
Kseniia Churiumova <kchuriumova@paypal.com>
2023-07-29 09:00:20 -0400

    Keep doing canary

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 
+import os
+
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return os.environ.get("TRACK", "Hello, World!")
 
 
 if __name__ == "__main__":

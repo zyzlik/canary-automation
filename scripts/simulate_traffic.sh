58d706c968d3ab2448b4a88d9dbd1b4f7e772bf7
Kseniia Churiumova <kchuriumova@paypal.com>
2023-09-03 09:18:15 -0400

    Change svc type

--- scripts/simulate_traffic.sh
+++ scripts/simulate_traffic.sh
@@ -1,7 +1,7 @@
 url=$(kubectl get svc envoy -n projectcontour -o jsonpath='{.status.loadBalancer.ingress[].hostname}')
 
-for _ in {1..30}
+for _ in {1..50}
 do
     curl $url
-    sleep 1
+    sleep 0.5
 done
